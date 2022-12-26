1d17628c89f98fddc09ea291af44c5071611a6e6
Alkhaled <almzrab@gmail.com>
2022-12-26 16:49:13 +0100

    add

--- pages/api/addTask.js
+++ pages/api/addTask.js
@@ -2,7 +2,7 @@ import { Tasks , dbConnection } from './model/taaskMoldel'
 
 const add = async (req , res )=>{
       dbConnection();
-      if (method === "OPTIONS") {
+      if (req.method === "OPTIONS") {
         return res.status(200).send("ok")
       }
 try{
